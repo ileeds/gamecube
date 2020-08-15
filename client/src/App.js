885687299b769aa6e774f405d15c572bd157e2ec
ileeds <ileeds@brandeis.edu>
2020-08-15 18:45:24 -0400

    init

---
+++ client/src/App.js
@@ -0,0 +1,180 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+import { capitalize, lowerCase, map } from 'lodash';
+
+import logo from './logo.svg';
+import './App.css';
+
+const Container = styled.div`
+  margin: 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const GameButton = styled.div`
+  border-radius: 100%;
+  text-align: center;
+  line-height: 80px;
+  width: 80px;
+  height: 80px;
+  background-color: ${({ pressed }) => pressed ? "green" : "gray" };
+`;
+
+const PlayerBlock = styled.div`
+  border-radius: 100%;
+  text-align: center;
+  line-height: 80px;
+  width: 80px;
+  height: 80px;
+  background-color: ${({ selected }) => selected ? "gray" : "green" };
+`;
+
+const ButtonText = styled.span`
+  line-height: normal;
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+class App extends Component {
+  state = {
+    players: null,
+    myPlayer: null,
+    responseToPost: '',
+    pressed: {
+      a: false,
+      b: false,
+      arrowup: false,
+      arrowdown: false,
+      arrowleft: false,
+      arrowright: false,
+    },
+  };
+  
+  componentDidMount() {
+    this.getPlayers().then(res => this.setState({ players: res.players }))
+    document.addEventListener("keydown", this.handleKeyDown);
+    document.addEventListener("keyup", this.handleKeyUp);
+  }
+
+  getPlayers = async () => {
+    const response = await fetch('/api/players');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    
+    return body;
+  };
+
+  handleKeyDown = async e => {
+    const key = lowerCase(e.key).replace(/\s/g, '');
+    console.log(key)
+    
+    if (this.state.pressed[key] !== undefined && !this.state.pressed[key]) {
+      this.setState({ pressed: {
+        ...this.state.pressed,
+        [key]: true,
+      }});
+  
+      const response = await fetch('/api/input', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          key: key,
+          press: true,
+          player: this.state.myPlayer,
+        }),
+      });
+      const body = await response.text();
+      
+      this.setState({ responseToPost: body });
+    }
+  };
+
+  handleKeyUp = async e => {
+    const key = lowerCase(e.key).replace(/\s/g, '');
+
+    if (this.state.pressed[key] !== undefined) {
+      this.setState({ pressed: {
+        ...this.state.pressed,
+        [key]: false,
+      }});
+  
+      const response = await fetch('/api/input', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          key: key,
+          press: false,
+          player: this.state.myPlayer,
+        }),
+      });
+      const body = await response.text();
+      
+      this.setState({ responseToPost: body });
+    }
+  };
+
+  selectPlayer = async key => {
+    const response = await fetch('/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        player: key,
+      }),
+    });
+    const body = await response.json();
+    console.log(body.players);
+
+    if (body.success) {
+      this.setState({ myPlayer: key });
+    }
+
+    this.setState({ players: body.players });
+  }
+
+  renderBody() {
+    if (this.state.myPlayer) {
+      return map(this.state.pressed, (val, key) => {
+        return (
+          <GameButton key={key} pressed={val}>
+            <ButtonText>{capitalize(key.replace(/arrow/g, ''))}</ButtonText>
+          </GameButton>
+        );
+      });
+    } else {
+      return map(this.state.players, (val, key) => {
+        return (
+          <PlayerBlock key={key} selected={val} onClick={() => this.selectPlayer(key)}>
+            <ButtonText>{`Player ${key}`}</ButtonText>
+          </PlayerBlock>
+        );
+      });
+    }
+  }
+  
+  render() {
+    console.log(this.state);
+
+    if (!this.state.players) {
+      return null;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Container>
+          {this.renderBody()}
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,110 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+const _ = require('lodash');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const players = {
+  1: false,
+  2: false,
+  3: false,
+};
+
+const stick = {
+  x: 0.5,
+  y: 0.5,
+}
+
+const getStickAction = () => {
+  return `SET MAIN ${stick.x} ${stick.y}`;
+}
+
+const path = '/Users/ianleeds/Library/Application Support/Dolphin/Pipes/pipe'.replace(/ /g, '\\ ');
+
+app.get('/api/players', (req, res) => {
+  res.send({ players });
+});
+
+app.post('/api/register', (req, res) => {
+  const { player } = req.body;
+  let success = false;
+  if (!players[player]) {
+    players[player] = true;
+    success = true;
+  }
+  res.send({ players, success });
+})
+
+app.post('/api/input', (req, res) => {
+  const { key, press, player } =  req.body;
+
+  const action = ((k, p) => {
+    switch(k) {
+      case 'a':
+        if (p) {
+          return 'PRESS A';
+        }
+        return 'RELEASE A';
+      case 'b':
+        if (p) {
+          return 'PRESS B';
+        }
+        return 'RELEASE B';
+      case 'arrowup':
+        if (p) {
+          stick.y = 0;
+          return getStickAction();
+        }
+        stick.y = 0.5;
+        return getStickAction();
+      case 'arrowdown':
+        if (p) {
+          stick.y = 1;
+          return getStickAction();
+        }
+        stick.y = 0.5;
+        return getStickAction();
+      case 'arrowleft':
+        if (p) {
+          stick.x = 0;
+          return getStickAction();
+        }
+        stick.x = 0.5;
+        return getStickAction();
+      case 'arrowright':
+        if (p) {
+          stick.x = 1;
+          return getStickAction();
+        }
+        stick.x = 0.5;
+        return getStickAction();
+      default:
+        return null;
+    }
+  })(key, press);
+
+  if (action) {
+    exec(`echo '${action}' > ${path}${player}`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(action);
+    });
+
+    res.send(
+      `${key}, ${press}, ${player}`,
+    );
+  }
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
